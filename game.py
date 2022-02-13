b09528283aef152a8cf09c15f5a829c1bfa8202e
pk558 <patrykoo575@gmail.com>
2022-02-13 21:31:35 +0100

    enemy and player collisions + optimalization

--- game.py
+++ game.py
@@ -1,10 +1,10 @@
-from sre_parse import HEXDIGITS
+
 import pygame
 import time
 import random
-import threading 
-WIDTH = 800
-HEIGHT = 600
+
+WIDTH = 1300
+HEIGHT = 700
 
 class Background(pygame.sprite.Sprite):
     def __init__(self, width: int, height: int, pos: tuple):
@@ -19,21 +19,32 @@ class Background(pygame.sprite.Sprite):
 
 
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self, pos: tuple, width: int, height: int, level: str):
+    def __init__(self, pos: tuple, width: int, height: int):
         super(Enemy, self).__init__()
         self.image = pygame.image.load("images/enemies/chicken_1.png").convert_alpha()
         self.image = pygame.transform.smoothscale(self.image, (width, height))
         self.rect = self.image.get_rect()
         self.rect.center = (pos[0] + random.randrange(0, WIDTH - self.image.get_width()), pos[1])
         self.prev_time = time.time()
-        self.speed = random.SystemRandom().uniform(.25, .5)
+        self.speed = random.SystemRandom().uniform(.25, .30)
         self.hp = 100
     def update(self):
+        self.is_player_hitted()
         now = time.time()
         dt = now - self.prev_time
         self.prev_time = now
         self.moving_speed = (self.speed * dt) * 1000
         self.rect.center = (self.rect.center[0], self.rect.center[1] + self.moving_speed)
+    def is_player_hitted(self):
+        for id, enemy in enumerate(new_game.enemies):
+            if new_game.checkCollision(enemy, new_game.player):
+                if new_game.player.player_lives > 0: 
+                    new_game.player.player_lives -= 1
+                    new_game.enemies.remove(enemy)
+                    enemy.kill()
+                else:
+                    print("game over")
+                
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, width: int, height: int, pos: tuple):
         super(Bullet, self).__init__()
@@ -45,11 +56,24 @@ class Bullet(pygame.sprite.Sprite):
         self.prev_time = time.time()
         self.speed = 2.5
     def update(self):
+        self.is_bullet_outside_screen()
+        self.is_enemy_hitted()
         now = time.time()
         dt = now - self.prev_time
         self.prev_time = now
         shoot_speed = (self.speed * dt) * 1000
         self.rect.center = (self.rect.center[0], self.rect.center[1] - shoot_speed)
+    def is_bullet_outside_screen(self):
+        for id, bullet in enumerate(new_game.bullets):
+            if bullet.rect.center[1] < 0:
+                new_game.bullets.remove(bullet)
+                new_game.player.bullet_group.remove(bullet)
+    def is_enemy_hitted(self):
+        for id, enemy in enumerate(new_game.enemies):
+            if new_game.checkCollision(enemy, self):
+                new_game.enemies.remove(enemy)
+                enemy.kill()
+        
 class Player(pygame.sprite.Sprite):
     def __init__(self, width: int, height: int, pos: tuple):
         super(Player, self).__init__()
@@ -58,9 +82,10 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = pos
         self.prev_time = time.time()
-        self.player_speed = 1
-        self.player_ammunition_max = 50
-        self.player_ammunition = 50
+        self.player_speed = 1.25
+        self.player_ammunition_max = 20000
+        self.player_ammunition = 20000
+        self.player_lives = 10
         self.bullet_group = pygame.sprite.RenderPlain()
     def update(self):
         keys = pygame.key.get_pressed()
@@ -89,6 +114,7 @@ class Player(pygame.sprite.Sprite):
                 self.player_ammunition -= 1
         if keys[pygame.K_r]:
             self.player_ammunition = self.player_ammunition_max
+
         self.bullet_group.draw(new_game.screen)
         self.bullet_group.update()
         
@@ -98,6 +124,7 @@ class Player(pygame.sprite.Sprite):
 class Game():
     def __init__(self, WIDTH, HEIGHT):
         pygame.init()
+        pygame.mixer.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         #time/deltatime
         self.prev_time = time.time()
@@ -118,10 +145,15 @@ class Game():
         self.enemies = []
         #run
         self.run = True
+        #images
+        self.bullet_image = pygame.image.load("images/bullets/01.png").convert_alpha()
+        self.hearth_image = pygame.transform.smoothscale(pygame.image.load("images/hearth.png").convert_alpha(), (int(WIDTH*.05), int(HEIGHT*.05)))
+        #sounds
+        self.shoot_sound = pygame.mixer.Sound("sounds/shoot_sound.wav")
     def Run(self):
         Level(5).update()
         while self.run:
-            #print(self.bullets)
+            print(len(self.enemies))
             keys = pygame.key.get_pressed()
             pygame.time.Clock().tick(144)
             for event in pygame.event.get():
@@ -132,26 +164,26 @@ class Game():
             #print(self.level_group)
             self.level_group.draw(self.screen)
             self.level_group.update()
-            
-
             if keys[pygame.K_q]:
                 Level(random.randrange(1, 15)).update()
+
+            if self.player.player_lives > 0:
+                self.screen.blit(self.hearth_image, (50, 50))
+                self.screen.blit(self.font.render(""+str(self.player.player_lives), True, (255,255,255)), (100,50))
             if self.player.player_ammunition > 0:
-                self.screen.blit(pygame.transform.rotate(pygame.image.load("images/bullets/01.png"), 90), (10, -40))
+                self.screen.blit(pygame.transform.rotate(self.bullet_image, 90), (10, -40))
                 self.screen.blit(self.font.render(""+str(self.player.player_ammunition), True, (255,255,255)), (100,10))
             elif self.player.player_ammunition == 0:
                 self.screen.blit(self.font.render("You have run out of ammo, press R to reload!", True, (255,255,255)), (0,0))
             pygame.display.update()
     def checkCollision(self, sprite1, sprite2):
-        col = pygame.sprite.collide_rect(sprite1, sprite2)
-        if col == True:
-            print("trafiono")
+        return pygame.sprite.collide_rect(sprite1, sprite2)
 class Level():
     def __init__(self, level):
         self.level = level
     def update(self):
         for i in range(0, self.level):
-            enemy = Enemy((0,0), int(WIDTH*.05), int(HEIGHT*.05), str(self.level))
+            enemy = Enemy((0,0), int(WIDTH*.04), int(WIDTH*.04))
             new_game.enemies.append(enemy)
             new_game.level_group.add(enemy)
 
